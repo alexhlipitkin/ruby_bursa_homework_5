8dc5aaedb1bce7a260e7ab3a9434beee41250118
alexhlipitkin <alex.hlipitkin@gmail.com>
2015-07-30 00:33:16 +0300

    o

---
+++ master/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Master::Application.config.secret_token = 'bef5cd806f1b770f710c72d813109f2112c20d2f2266addf45bfe55305c4cdaf1d6dbe494f98bf94da6113cd3c907a951d21431dd524192f9799142f139c3a74'
